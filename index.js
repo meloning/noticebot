909aba912fbde78907cb648ec0016fe0b0d1e1b6
meloning <melon8372@gmail.com>
2019-10-18 08:13:29 +0900

    feature: Add redis connect and refactoring function

--- index.js
+++ index.js
@@ -1,46 +1,67 @@
 const client = require('cheerio-httpcli');
-
-// TODO: Redis DB Connect
-
 let url = 'https://work.mma.go.kr/caisBYIS/board/boardList.do?menu_id=m_m8_6&tmpl_id=1&gesipan_gbcd=13';
 let param = {};
+
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+const TelegramBot = require('node-telegram-bot-api');
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '988819272:AAFTQDQbO0DRY8HrWoxavBoMFGu4ksg4moU';
+const chatId = 981635602;
+const bot = new TelegramBot(token, {polling: true});
+
+const cron = require('node-cron');
+
 let tempDate = '';
 
-client.fetch(url, param, (err, $, res) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    let todayDate = new Date();
-
-    if (dateToString(todayDate) !== dateToString(tempDate)) {
-        tempDate = todayDate;
-        // TODO: DB Count 초기화, log에 초기화한 내역 기록
-    }
-
-    let newPostCount = 0;
-
-    $(".brd_list_n > tbody > tr > td").each(function (index, element) {
-        if ($(this).find('img').attr('alt') === '새글') {
-            let date = $(this).next().next().text();
-            if (date === dateToString(todayDate)) {
-                newPostCount++;
-                // TODO: DB 값과 비교  newPostCount > DB Value -> Telegram push(log 기록)
-            }
-        }
-    });
-    console.log(newPostCount);
-});
-
-function dateToString(format) {
-    let year = format.getFullYear();
-
-    let month = format.getMonth() + 1;
-    if(month<10) month = '0' + month;
-
-    let date = format.getDate();
-    if(date<10) date = '0' + date;
-
-    return year + "-" + month + "-" + date;
+function sendMessageBot() {
+    bot.sendMessage(chatId,"[JunsuAlert]");
+    console.log("[INFO] SUCCESS Send Message([JunsuAlert])");
 }
+cron.schedule('*/30 * * * * *', function () {
+    console.log("[INFO} ================== Cron Work ==================");
+    client.fetch(url, param, (err, $) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        let todayDate = new Date();
+        let newPostCount = 0;
+
+        // format Date
+        let year = todayDate.getFullYear();
+        let month = todayDate.getMonth() + 1;
+        if (month < 10) month = '0' + month;
+        let date = todayDate.getDate();
+        if (date < 10) date = '0' + date;
+
+        let formattedDate = year + "-" + month + "-" + date;
+
+        // Init todayCount key when Date changed.
+        if (formattedDate !== tempDate) {
+            tempDate = formattedDate;
+            redisClient.set('todayCount', 1);
+            console.log("[INFO] The value of todayCount is initialized because the date(" + formattedDate + ") has changed.")
+        }
+
+        $(".brd_list_n > tbody > tr > td").each(function () {
+            if ($(this).find('img').attr('alt') === '새글') {
+                let date = $(this).next().next().text();
+                if (date === formattedDate) {
+                    newPostCount++;
+                }
+            }
+        });
+        console.log("[INFO] newPostCount:" + newPostCount);
+
+        redisClient.get('todayCount', (err, reply) => {
+            console.log("[INFO] Redis todayCount:" + reply);
+            if (newPostCount > reply) {
+                redisClient.set('todayCount', newPostCount);
+                sendMessageBot();
+            }
+        });
+    });
+});
